eecf1769873f6259ad014272d0e78ce8388cf00d
andersseen <andriipap01@gmail.com>
2024-04-13 16:25:17 +0200

    Add prisma

---
+++ prisma/migrations/20240413142413_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Client" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "phone" TEXT,
+    "email" TEXT,
+    "notes" TEXT
+);
